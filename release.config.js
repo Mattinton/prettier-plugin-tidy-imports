2cbe5e9d9417d0d81e55fd9531c65f2fe4c2ec3d
Matthew Kinton <matthew@fkinton.com>
2021-11-28 19:40:36 +0000

    ci(release): use main as release branch

--- release.config.js
+++ release.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  release: {
-    branches: [
-      "+([0-9])?(.{+([0-9]),x}).x",
-      "main",
-      "next",
-      "next-major",
-      { name: "beta", prerelease: true },
-      { name: "alpha", prerelease: true },
-    ],
-  },
+  defaultBranch: "main",
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
 };
